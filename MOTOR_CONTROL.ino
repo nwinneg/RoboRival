8bc631fecb4d148d810f7f8d052b57061c304c01
Troy Gu <92765409+troygu2@users.noreply.github.com>
2021-11-07 19:22:15 -0500

    Updated FSM, MOTORCONTROL and SERVO
    Added more actions in FSM,
    Updated PINOUT in MOTORCONTROL,
    Updated home location in SERVO.

---
+++ MOTOR_CONTROL.ino
@@ -0,0 +1,68 @@
+/*
+Nate Winneg
+RoboRival  
+    
+*/
+
+/* Includes -------------------------------------------------------*/
+
+/* Macros ---------------------------------------------------------*/
+#define MOTORPIN_RL    2 
+#define DIRCONTROLRL  50
+#define MOTORPIN_RR    3 
+#define DIRCONTROLRR  51
+#define MOTORPIN_FL    5 
+#define DIRCONTROLFL  53
+#define MOTORPIN_FR    4 
+#define DIRCONTROLFR  52 
+
+/* Functions ------------------------------------------------------*/
+
+void setupMotors(void)
+{
+  pinMode(MOTORPIN_RL, OUTPUT); pinMode(MOTORPIN_RR, OUTPUT);
+  pinMode(MOTORPIN_FL, OUTPUT); pinMode(MOTORPIN_FR, OUTPUT);
+
+  pinMode(DIRCONTROLRL, OUTPUT); pinMode(DIRCONTROLRR, OUTPUT);
+  pinMode(DIRCONTROLFL, OUTPUT); pinMode(DIRCONTROLFR, OUTPUT);
+}
+
+// set all motors to the same speed
+void setMotorPWM_All(float dutyCycle)
+{
+  float motorSetting = (dutyCycle/100)*255;
+  digitalWrite(MOTORPIN_RL, motorSetting);
+  digitalWrite(MOTORPIN_RR, motorSetting);
+  digitalWrite(MOTORPIN_FL, motorSetting);
+  digitalWrite(MOTORPIN_FR, motorSetting);
+
+  digitalWrite(DIRCONTROLRL, HIGH);
+  digitalWrite(DIRCONTROLRR, HIGH);
+  digitalWrite(DIRCONTROLFL, HIGH);
+  digitalWrite(DIRCONTROLFR, HIGH);
+}
+
+
+// set left motors to the same speed
+void setMotorPWM_Left(float dutyCycle)
+{
+  float motorSetting = (dutyCycle/100)*255;
+  digitalWrite(MOTORPIN_RL, motorSetting);
+  digitalWrite(MOTORPIN_FL, motorSetting);
+}
+
+// set right motors to the same speed
+void setMotorPWM_Right(float dutyCycle)
+{
+  float motorSetting = (dutyCycle/100)*255;
+  digitalWrite(MOTORPIN_RR, motorSetting);
+  digitalWrite(MOTORPIN_FR, motorSetting);
+}
+
+void setMotorSTOP()
+{
+  digitalWrite(DIRCONTROLRL, LOW);
+  digitalWrite(DIRCONTROLRR, LOW);
+  digitalWrite(DIRCONTROLFL, LOW);
+  digitalWrite(DIRCONTROLFL, LOW);
+}
